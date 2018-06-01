1f365287bd3d86f46d2c1cedff7021e7dceadfb0
Wu oneness629 <oneness629@hotmail.com>
2018-06-01 15:25:09 +0800

    添加未翻译文本写入到txt文件 添加新零件文件 编译新文件

--- Subtitle/MSCTranslateChs/MSCTranslateChs/MSCTranslateChs.cs
+++ Subtitle/MSCTranslateChs/MSCTranslateChs/MSCTranslateChs.cs
@@ -25,9 +25,13 @@ namespace MSCTranslateChs
         public bool IsLoadResources = false;
         public bool IsLoadGameObject = false;
 
-        public List<string> subtitlesList;
-        public List<string> interactionsList;
-        public List<string> partnamesList;
+        public List<string> subtitlesList = new List<string>();
+        public List<string> interactionsList = new List<string>();
+        public List<string> partnamesList = new List<string>();
+
+        private int subtitlesListSize = 0;
+        private int interactionsListSize = 0;
+        private int partnamesListSize = 0;
 
         private string notTranslateString = "未翻译文本";
 
@@ -38,7 +42,6 @@ namespace MSCTranslateChs
         private TextMesh partnamesTextMesh;
         private GUIStyle partnamesGuiStyle;
         private Rect partnamesRect;
-
 
 
 
@@ -62,25 +65,76 @@ namespace MSCTranslateChs
             subtitlesList = File.ReadAllLines(Path.Combine(ModLoader.GetModAssetsFolder(this), "subtitles.txt")).ToList();
             interactionsList = File.ReadAllLines(Path.Combine(ModLoader.GetModAssetsFolder(this), "interactions.txt")).ToList();
             partnamesList = File.ReadAllLines(Path.Combine(ModLoader.GetModAssetsFolder(this), "partnames.txt")).ToList();
+
+            subtitlesListSize = subtitlesList.Count;
+            interactionsListSize = interactionsList.Count;
+            partnamesListSize = partnamesList.Count;
+
+            ModConsole.Print("翻译文本列表数量");
+            ModConsole.Print("subtitlesListSize :" + subtitlesListSize);
+            ModConsole.Print("interactionsListSize :" + interactionsListSize);
+            ModConsole.Print("partnamesListSize :" + partnamesListSize);
+
             IsLoadResources = true;
+        }
+
+        private void checkAndWriteTranslateText()
+        {
+            if (subtitlesListSize > 0 && subtitlesListSize < subtitlesList.Count)
+            {
+                File.WriteAllLines(Path.Combine(ModLoader.GetModAssetsFolder(this), "subtitles.txt"), subtitlesList.ToArray());
+                subtitlesListSize = subtitlesList.Count;
+                ModConsole.Print("新的未翻译文本已写入到subtitles.txt");
+            }
+            if (interactionsListSize > 0 && interactionsListSize < interactionsList.Count)
+            {
+                File.WriteAllLines(Path.Combine(ModLoader.GetModAssetsFolder(this), "interactions.txt"), interactionsList.ToArray());
+                interactionsListSize = interactionsList.Count;
+                ModConsole.Print("新的未翻译文本已写入到interactions.txt");
+            }
+            /*
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                ModConsole.Print("partnamesListSize: " + partnamesListSize);
+                ModConsole.Print("partnamesList.Count: " + partnamesList.Count);
+                ModConsole.Print("partnamesListSize > 0: " + (partnamesListSize > 0));
+                ModConsole.Print("partnamesListSize > partnamesList.Count : " + (partnamesListSize  partnamesList.Count));
+                ModConsole.Print("partnamesListSize > 0 && partnamesListSize > partnamesList.Count : " + (partnamesListSize > 0 && partnamesListSize > partnamesList.Count));
+                ModConsole.Print(partnamesListSize > 0 && partnamesListSize > partnamesList.Count);
+            }
+            */
+            if (partnamesListSize > 0 && partnamesListSize < partnamesList.Count)
+            {
+                File.WriteAllLines(Path.Combine(ModLoader.GetModAssetsFolder(this), "partnames.txt"), partnamesList.ToArray());
+                partnamesListSize = partnamesList.Count;
+                ModConsole.Print("新的未翻译文本已写入到partnames.txt");
+            }
         }
 
         public override void OnGUI()
         {
-
-            if (IsLoadResources && IsLoadGameObject && Application.loadedLevelName == "GAME")
+            try
             {
-                string subtitlesText = subtitlesTextMesh.text.Trim();
-                if (subtitlesTextMesh.gameObject.activeSelf && !string.IsNullOrEmpty(subtitlesText))
+                if (IsLoadResources && IsLoadGameObject && Application.loadedLevelName == "GAME")
                 {
-                    GUI.Label(subtitlesRect, TranslateString(subtitlesText, subtitlesList), subtitlesGuiStyle);
-                }
-                string partnamesText = partnamesTextMesh.text.Trim();
-                if (partnamesTextMesh.gameObject.activeSelf && !string.IsNullOrEmpty(partnamesText))
-                {
-                    GUI.Label(partnamesRect, TranslateString(partnamesText, partnamesList), partnamesGuiStyle);
+                    string subtitlesText = subtitlesTextMesh.text.Trim();
+                    if (subtitlesTextMesh.gameObject.activeSelf && !string.IsNullOrEmpty(subtitlesText))
+                    {
+                        GUI.Label(subtitlesRect, TranslateString(subtitlesText, subtitlesList), subtitlesGuiStyle);
+                    }
+                    string partnamesText = partnamesTextMesh.text.Trim();
+                    if (partnamesTextMesh.gameObject.activeSelf && !string.IsNullOrEmpty(partnamesText))
+                    {
+                        GUI.Label(partnamesRect, TranslateString(partnamesText, partnamesList), partnamesGuiStyle);
+                    }
+                    checkAndWriteTranslateText();
                 }
             }
+            catch(Exception e)
+            {
+                ModConsole.Print("GUI异常: " + e.Message);
+            }
+            
         }
 
 
@@ -110,17 +164,20 @@ namespace MSCTranslateChs
         {
             if (string.IsNullOrEmpty(text))
             {
-                return text;
+                return "\"\"";
             }
             string listText = textList.FirstOrDefault((string s) => s.ToUpper().Contains(text.Trim().ToUpper()));
             if (string.IsNullOrEmpty(listText))
             {
                 textList.Add(text + "=" + notTranslateString);
                 ModConsole.Print("文本在列表中未找到: " + text);
-                    
+                return notTranslateString;
             }
             string resultString = listText.Split('=')[1];
-            resultString = resultString.Replace("\\n","\n");
+            if (!string.IsNullOrEmpty(resultString))
+            {
+                resultString = resultString.Replace("\\n", "\n");
+            }
             return resultString;
 
 
