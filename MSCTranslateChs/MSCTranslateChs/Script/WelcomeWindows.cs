23f974066bb1fadbb11f9274199bd2c7fae7b77b
Wu oneness629 <oneness629@hotmail.com>
2018-09-03 13:58:07 +0800

    添加游戏内时间测试

--- MSCTranslateChs/MSCTranslateChs/Script/WelcomeWindows.cs
+++ MSCTranslateChs/MSCTranslateChs/Script/WelcomeWindows.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HutongGames.PlayMaker;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,10 +17,17 @@ namespace MSCTranslateChs.Script
         Vector2 scrollPoint;
         int windowsId = 629;
 
+        FsmFloat timeRotationMinute;
+        FsmFloat timeRotationHour;
+        FsmFloat globalTime;
+
         public WelcomeWindows(MSCTranslateChs mscTranslateChs)
         {
             welcomeWindowsRect = new Rect(Screen.width / 2 - windowsWidth / 2, Screen.height / 2 - windowsHeight / 2, windowsWidth, windowsHeight);
             this.mscTranslateChs = mscTranslateChs;
+            timeRotationMinute = FsmVariables.GlobalVariables.FindFsmFloat("TimeRotationMinute");
+            timeRotationHour = FsmVariables.GlobalVariables.FindFsmFloat("TimeRotationHour");
+            globalTime = FsmVariables.GlobalVariables.FindFsmFloat("GlobalTime");
         }
 
 
@@ -37,11 +45,13 @@ namespace MSCTranslateChs.Script
         {
             scrollPoint = GUILayout.BeginScrollView(scrollPoint);
             GUILayout.Label("  欢迎使用我的夏季汽车中文翻译Mod");
+            GUILayout.Label("      游戏内时间:globalTime -> " + globalTime + " " + Mathf.FloorToInt(timeRotationHour.Value / (360 / 12)) + ":" + Mathf.FloorToInt(timeRotationMinute.Value / (360 / 60)));
             GUILayout.Label("  当前翻译内容：状态UI、商品动作、配件、字幕(需要打开英文字幕),中英文对照显示，不会覆盖原有英文内容。");
             GUILayout.Label("      状态UI、商品动作、配件、字幕(需要打开英文字幕),中英文对照显示，不会覆盖原有英文内容。");
             GUILayout.Label("      详细使用教程请参考steam中文模组指南(界面比较乱，以后慢慢改~)");
             GUILayout.Label("      显示/隐藏本窗口（左边ALT+W）/（右边ALT+W）");
             GUILayout.Label("      启用GameOver提示后，玩家Over后报纸内容显示出Over原因时按下左边ALT+G，一次性读取全部原因到gameover.txt");
+            
 
             mscTranslateChs.IsEnable = GUILayout.Toggle(mscTranslateChs.IsEnable, "是否启用翻译Mod（取消，会加载文本到内存，但不会进行任何OnGUI交互）");
             mscTranslateChs.IsTranslateGameOverMessage = GUILayout.Toggle(mscTranslateChs.IsTranslateGameOverMessage, "是否启用GameOver提示文本（报刊上的GameOver原因）");
@@ -54,7 +64,6 @@ namespace MSCTranslateChs.Script
             
             mscTranslateChs.IsDevelop = GUILayout.Toggle(mscTranslateChs.IsDevelop, "是否启用开发模式（左边Alt+T（显示）和右边Alt+T（隐藏））");
 
-            isShowWelcomeWindows = GUILayout.Toggle(isShowWelcomeWindows, "下次启动是否再次显示窗口（这个没用的···我还没写完···慢慢来···）");
             if (GUILayout.Button("关闭 或 按XXX按键关闭")){
                 mscTranslateChs.isShowWelcomeWindows = false;
             }
