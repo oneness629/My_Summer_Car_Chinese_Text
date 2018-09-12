cc428aa3b61f6de6aa72e9985305e80940ae7acc
Wu oneness629 <oneness629@hotmail.com>
2018-09-12 14:19:35 +0800

    添加生命值和状态显示

--- MSCTranslateChs/MSCTranslateChs/Script/Module/CarReport.cs
+++ MSCTranslateChs/MSCTranslateChs/Script/Module/CarReport.cs
@@ -13,6 +13,9 @@ namespace MSCTranslateChs.Script.Module
         private static readonly LOGGER logger = new LOGGER(typeof(CarReport));
         public static readonly string BOLT_COUNT = "bolt_count";
         public static readonly string PART_AND_BOLT = "part_and_bolt";
+        public static readonly string PART_ATTR = "part_attr";
+        public static readonly string FSM_FLOAT_TIGHTNESS = "fsm_float_tightness";
+        public static readonly string FSM_FLOAT_TIREHEALTH = "fsm_float_tirehealth";
 
         public override string ModuleComment { get => "车辆报告"; }
         
@@ -81,6 +84,16 @@ namespace MSCTranslateChs.Script.Module
                 {
                     partAndBoltDict.Add(CarReport.PART_AND_BOLT, partDict);
                 }
+                // 部件GameObject -> 其它属性
+                Dictionary<GameObject, Dictionary<string, object>> partAttrDict = new Dictionary<GameObject, Dictionary<string, object>>();
+                if (partAndBoltDict.ContainsKey(CarReport.PART_ATTR))
+                {
+                    partAndBoltDict[CarReport.PART_ATTR] = partAttrDict;
+                }
+                else
+                {
+                    partAndBoltDict.Add(CarReport.PART_ATTR, partAttrDict);
+                }
 
 
                 foreach (GameObject booltPmGameObject in allBoltPmGameObjectList)
@@ -94,12 +107,57 @@ namespace MSCTranslateChs.Script.Module
                         }
                         else
                         {
-                            partDict.Add(partFsmGameObject.Value , new List<GameObject>() { booltPmGameObject });
+                            partDict.Add(partFsmGameObject.Value, new List<GameObject>() { booltPmGameObject });
                         }
                     }
                 }
 
 
+                foreach (GameObject partGameObject in partDict.Keys)
+                {
+                    PlayMakerFSM[] partAssembledPlayMakerFSMArray = partGameObject.GetComponents<PlayMakerFSM>();
+                    FsmFloat fsmFloatTightness = null;
+                    FsmFloat fsmFloatTireHealth = null;
+                    Dictionary<string, object> partAttrDataDict = new Dictionary<string, object>();
+                    foreach (PlayMakerFSM playMakerFSM in partAssembledPlayMakerFSMArray)
+                    {
+                        if (playMakerFSM != null && playMakerFSM.FsmName != null &&
+                                (
+                                playMakerFSM.FsmName.ToLower().Equals("use") ||
+                                playMakerFSM.FsmName.ToLower().Equals("boltcheck")
+                                )
+                            )
+                        {
+                            fsmFloatTightness = playMakerFSM.FsmVariables.FindFsmFloat("Tightness");
+                            fsmFloatTireHealth = playMakerFSM.FsmVariables.FindFsmFloat("TireHealth");
+                        }
+                    }
+                    if (partAttrDict.ContainsKey(partGameObject))
+                    {
+                        partAttrDict[partGameObject] = partAttrDataDict;
+                    }
+                    else
+                    {
+                        partAttrDict.Add(partGameObject, partAttrDataDict);
+                    }
+                    if (partAttrDataDict.ContainsKey(CarReport.FSM_FLOAT_TIGHTNESS))
+                    {
+                        partAttrDataDict[CarReport.FSM_FLOAT_TIGHTNESS] = fsmFloatTightness;
+                    }
+                    else
+                    {
+                        partAttrDataDict.Add(CarReport.FSM_FLOAT_TIGHTNESS, fsmFloatTightness);
+                    }
+                    if (partAttrDataDict.ContainsKey(CarReport.FSM_FLOAT_TIREHEALTH))
+                    {
+                        partAttrDataDict[CarReport.FSM_FLOAT_TIREHEALTH] = fsmFloatTireHealth;
+                    }
+                    else
+                    {
+                        partAttrDataDict.Add(CarReport.FSM_FLOAT_TIREHEALTH, fsmFloatTireHealth);
+                    }
+
+                }
             }
 
         }
@@ -125,6 +183,7 @@ namespace MSCTranslateChs.Script.Module
                     {
                         int boltCount = Convert.ToInt16(partAndBoltDict[CarReport.BOLT_COUNT]);
                         Dictionary<GameObject, List<GameObject>> partDict = partAndBoltDict[CarReport.PART_AND_BOLT] as Dictionary<GameObject, List<GameObject>>;
+                        Dictionary<GameObject, Dictionary<string, object>> partAttrDict = partAndBoltDict[CarReport.PART_ATTR] as Dictionary<GameObject, Dictionary<string, object>>;
 
                         GUILayout.Label("已安装部件一共有个" + boltCount + "螺栓/丝");
 
@@ -135,6 +194,33 @@ namespace MSCTranslateChs.Script.Module
                             if (boltPmGameObjectList != null)
                             {
                                 GUILayout.Label("该部件有" + boltPmGameObjectList .Count + "个螺栓/丝");
+
+                                if (partAttrDict.ContainsKey(partGameObject))
+                                {
+                                    Dictionary<string, object> partAttrDataDict = partAttrDict[partGameObject];
+                                    if (partAttrDataDict.ContainsKey(CarReport.FSM_FLOAT_TIGHTNESS) && partAttrDataDict[CarReport.FSM_FLOAT_TIGHTNESS] != null)
+                                    {
+                                        FsmFloat fsmFloatTightness = partAttrDataDict[CarReport.FSM_FLOAT_TIGHTNESS] as FsmFloat;
+
+                                        GUILayout.Label("部件螺栓锁定状态" + fsmFloatTightness.Value + " -> 完全锁紧需要值 -> " + (8 * boltPmGameObjectList.Count) );
+                                        if (GUILayout.Button("锁紧"))
+                                        {
+                                            fsmFloatTightness.Value = 8 * boltPmGameObjectList.Count;
+                                        }
+                                    }
+                                    if (partAttrDataDict.ContainsKey(CarReport.FSM_FLOAT_TIREHEALTH) && partAttrDataDict[CarReport.FSM_FLOAT_TIREHEALTH] != null)
+                                    {
+                                        FsmFloat fsmFloatTireHealth = partAttrDataDict[CarReport.FSM_FLOAT_TIREHEALTH] as FsmFloat;
+                                        GUILayout.Label("部件生命值:" + fsmFloatTireHealth.Value);
+
+                                    }
+                                }
+                                else
+                                {
+                                    GUILayout.Label("未读取到该部件其它属性");
+                                }
+                                
+
                             }
                             else
                             {
